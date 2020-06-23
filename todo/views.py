52fb1457fcb55d3a190c0fe7791a3954aa10bb24
Joshua <Joshua.Golde@hs-augsburg.de>
2020-06-23 20:15:33 +0200

    basic todoScreen with template

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todoView(request):
-    return HttpResponse('hello, this is the todoView.')
+    return render(request, 'todo.html')
