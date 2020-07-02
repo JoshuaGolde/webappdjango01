97748e2b9ebbaba72ac9e5200697c661912902f8
Joshua <Joshua.Golde@hs-augsburg.de>
2020-07-03 01:25:27 +0200

    logout page styled

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('', views.login),
     path('signup', views.signup, name='signup'),
     path('login', auth_views.LoginView.as_view(template_name='authenticate/login2.html'), name='login2'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout', auth_views.LogoutView.as_view(template_name='authenticate/logout.html'), name='logout'),
     path('', views.login, name='login'),
 ]
