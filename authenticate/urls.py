2c074aa49f076b6b1d48692b94011e1c8ad7abc7
Joshua <Joshua.Golde@hs-augsburg.de>
2020-07-03 01:04:19 +0200

    temporary second login for code tryout

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -6,5 +6,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     # path('', views.login),
     path('signup', views.signup, name='signup'),
+    path('login', auth_views.LoginView.as_view(template_name='authenticate/login2.html'), name='login2'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.login, name='login'),
 ]
